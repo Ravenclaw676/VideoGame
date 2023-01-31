dc246fb6832bfd9927bafe61594621ff0f3549c6
Ravenclaw676 <39599752+Ravenclaw676@users.noreply.github.com>
2023-01-31 16:57:09 +0000

    more enemy stuff

--- Item.py
+++ Item.py
@@ -20,3 +20,10 @@ class Weapon(Item):
     def __init__(self, name, weight, damage):
         self.damage = damage
         super().__init__(name, weight)
+    
+    def attack(self, target):
+        target.set_health(target.get_health() - self.damage)
+        if target.get_health() == 0:
+            target.get_location().remove_enemy(target)
+            print("The enemy is dead")
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Room import Room
-from Entity import Player
+from Entity import Entity, Player
 from Item import Weapon
 
 sword = Weapon("sword", 15, 10)
@@ -18,7 +18,10 @@ dining_hall.link_room(kitchen, "north")
 
 dining_hall.link_room(ballroom, "west")
 ballroom.link_room(dining_hall, "east")
-ballroom.add_item(sword)
+dining_hall.add_item(sword)
+
+boss = Entity("boss", ballroom, 10)
+ballroom.add_enemy(boss)
 
 player1 = Player(kitchen, 999, 10)
 
@@ -34,6 +37,8 @@ while not quitting or ballroom.get_total_enemies() > 0:
         case "move":
             direction = input("Please enter the direction: ")
             player1.move(direction)
+        case "attack":
+            player1.weapon.attack(player1.location.enemies[0])
         case "quit":
             quitting = True
             continue
