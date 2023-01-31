bfc1335fe68e656e4b7ae39de8c63efd09aa7bea
Ravenclaw676 <39599752+Ravenclaw676@users.noreply.github.com>
2023-01-31 17:19:33 +0000

    fixed

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from Room import Room
-from Entity import Player
+from Entity import Entity, Player
 from Item import Weapon
 
+sword = Weapon("sword", 15, 10)
 
-def main():
-    sword = Weapon("sword", 15, 10)
+kitchen = Room("kitchen")
+kitchen.set_description("A kitchen")
 
-    kitchen = Room("kitchen")
-    kitchen.set_description("A kitchen")
+dining_hall = Room("Dining hall")
+dining_hall.set_description("you eat food here bro")
 
-    dining_hall = Room("Dining hall")
-    dining_hall.set_description("you eat food here bro")
+ballroom = Room("Ballroom")
+ballroom.set_description("you do be dancing bro")
 
-    ballroom = Room("Ballroom")
-    ballroom.set_description("you do be dancing bro")
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
 
-    kitchen.link_room(dining_hall, "south")
-    dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+dining_hall.add_item(sword)
 
-    dining_hall.link_room(ballroom, "west")
-    ballroom.link_room(dining_hall, "east")
-    ballroom.add_item(sword)
+boss = Entity("boss", ballroom, 10)
+ballroom.add_enemy(boss)
 
-    player1 = Player(kitchen, 999, 10)
+player1 = Player(kitchen, 999, 10)
 
 quitting = False
 while not quitting or ballroom.get_total_enemies() > 0:
@@ -36,19 +37,17 @@ while not quitting or ballroom.get_total_enemies() > 0:
         case "move":
             direction = input("Please enter the direction: ")
             player1.move(direction)
+        case "attack":
+            player1.weapon.attack(player1.location.enemies[0])
         case "quit":
             quitting = True
             continue
         case _:
             print("Invalid input")
 
-        print(player1)
+    print(player1)
 
-    if quitting:
-        print("You quit")
-    else:
-        print("your won")
-
-
-if __name__ == "__main__":
-    main()
+if quitting:
+    print("You quit")
+else:
+    print("your won")
